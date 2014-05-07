8b6fea8b7b27dd507c01a351d5931afc0e3512a9
diffsky <matthew.hadley@gmail.com>
2014-05-07 00:20:26 -0700

    example using rawgit

--- example/index.js
+++ example/index.js
@@ -6,9 +6,10 @@ var path = require('path');
 var hifile = require('..');
 var head = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">\
 <title>hifile</title><style>body {margin: 0}</style>\
-<link rel="stylesheet" type="text/css" href="/assets/hifile.css">\
-<link rel="stylesheet" type="text/css" href="/assets/hljs-theme-solarized-dark.css">\
-<script src="/assets/hifile.js" type="text/javascript"></script></head>\
+<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/necolas/normalize.css/071af828a2f6c882d5de1536135bf13b5711f554/normalize.css">\
+<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/diffsky/hifile/cdbb7ab33c3d848720f1e6510b0dba96a5100dd9/assets/hifile.css">\
+<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/diffsky/hifile/5fc76f8f1934dff1f5f845d1fabfd7c60a0949c7/assets/hljs-theme-solarized-dark.css">\
+<script src="https://cdn.rawgit.com/diffsky/hifile/cdbb7ab33c3d848720f1e6510b0dba96a5100dd9/assets/hifile.js"></script></head>\
 <script>window.onload = function(){hifile.init()};</script><body>';
 var foot = '</body></html>';
 var code;
