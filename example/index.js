f365ffb96005bc8c8f4828edeb136a96cf691490
diffsky <matthew.hadley@gmail.com>
2014-05-07 00:31:49 -0700

    update example

--- example/index.js
+++ example/index.js
@@ -9,8 +9,8 @@ var head = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">\
 <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/necolas/normalize.css/071af828a2f6c882d5de1536135bf13b5711f554/normalize.css">\
 <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/diffsky/hifile/cdbb7ab33c3d848720f1e6510b0dba96a5100dd9/assets/hifile.css">\
 <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/diffsky/hifile/5fc76f8f1934dff1f5f845d1fabfd7c60a0949c7/assets/hljs-theme-solarized-dark.css">\
-<script src="https://cdn.rawgit.com/diffsky/hifile/b3a990b5cbf73274e38132c6b33917a87479af7e/assets/hifile.js"></script></head>\
-<script>window.onload = function(){hifile.init()};</script><body>';
+<script src="https://cdn.rawgit.com/diffsky/hifile/b3a990b5cbf73274e38132c6b33917a87479af7e/assets/hifile.js"></script>\
+<script>window.onload = function(){hifile.init()};</script></head><body>';
 var foot = '</body></html>';
 var code;
 var input = './index.js';
